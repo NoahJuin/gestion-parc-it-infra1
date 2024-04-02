7c91602485fa436d27b1152a45372208adfebc68
NoahJuin <150364822+NoahJuin@users.noreply.github.com>
2024-04-02 15:59:55 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -46,4 +46,11 @@ resource "azurerm_app_service" "webapp" {
     java_version    = "1.7"
     java_container  = "TOMCAT"
   }
+site_config {
+    application_stack {
+        java_server = "JAVA"
+        java_version = "java17"
+        java_server_version = "17"}}
+
 }
+
